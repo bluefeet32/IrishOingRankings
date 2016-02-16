896ae1b22ee819aafa824a5f574953b1b1e0da0c
Ruairi Short <ruairi.short@ichec.ie>
2016-02-16 23:14:17 +0000

    removed lots of printing and the legacy code

--- resParse.cc
+++ resParse.cc
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <assert.h>
 
 #include "athlete.h"
 
@@ -13,7 +14,6 @@ using namespace std;
 int main( void ) {
     ifstream dbFile ("pointsDB");
     string line;
-    vector<string> fullName;
     vector<string> name;
     int i, noEntries;
     // count the number of lines, i.e. number of athletes in the DB
@@ -40,11 +40,11 @@ int main( void ) {
     int lineNo = 0;
     while ( dbFile ) {
         if ( !getline( dbFile, line ) ) break;
-        cout << "lineNo " << lineNo << ": " << line << endl;
+//        cout << "lineNo " << lineNo << ": " << line << endl;
 
         istringstream ss( line );
-        vector<float> tmpPoints;
         int col = 0;
+        string name;
         while ( ss ) {
             string s;
             // Read a string with ; as the delimiter between strings
@@ -53,15 +53,13 @@ int main( void ) {
             switch ( col ) {
                 // column 0 is the surname
                 case 0:
-                    name.push_back( s );
-                    fullName.push_back( s );
+                    name = s;
                     break;
                 // column 1 is the forename
                 case 1:
-                    tmpAth.modifyName( s, name[lineNo] );
-                    fullName[lineNo] += ';' + s;
-                    name[lineNo] += s;
-                    cout << "name " << name[lineNo] << endl; //points[i];
+                    tmpAth.modifyName( s, name );
+                    name += s;
+//                    cout << "name " << name << endl; //points[i];
                     break;
                 // column 2 is the athletes current ranking score
                 // Note that there is no break as we want give the athlete these points
@@ -74,10 +72,6 @@ int main( void ) {
                     if ( col != 2 ) {
                         tmpAth.givePoints( stof ( s ) );
                     }
-// FIXME Why do I need tmpPoints as well as tmpAth?
-// I don't only here for testing
-                    tmpPoints.push_back( stof( s ) );
-                    cout << "points " << tmpPoints[col-2] << endl; //points[i];
                     break;
                 default:
                     cout << "Too many columns!" << endl;
@@ -87,8 +81,6 @@ int main( void ) {
         // clean up the temporary data structures
         athList.push_back( tmpAth );
         tmpAth.clear();
-        points.push_back( tmpPoints );
-        tmpPoints.clear();
         lineNo++;
     }
 
@@ -99,6 +91,7 @@ int main( void ) {
     for ( int i = 0; i < athList.size(); i++ ) {
         athList[i].print();
     }
+    cout << "\n";
 //    cout << "points " << points[0][0] << " " << points[0][1] << endl; //points[i];
 
 //FIXME need to pass an argument to set what this file is
@@ -111,7 +104,7 @@ int main( void ) {
     vector<float> racePoints; // Vector of the points of the athletes for a race
     while ( resFile ) {
         if ( !getline(resFile, line) ) break;
-        cout << "lineNo " << lineNo << ": " << line << endl;
+//        cout << "lineNo " << lineNo << ": " << line << endl;
 
         istringstream ss( line );
         int col = 0;
@@ -127,7 +120,7 @@ int main( void ) {
                     break;
                 case 3:
                     raceName[lineNo] += s;
-                    cout << "racename " << raceName[lineNo] << endl; //points[i];
+//                    cout << "racename " << raceName[lineNo] << endl; //points[i];
                     break;
                 default:
                     break;
@@ -139,46 +132,17 @@ int main( void ) {
 
     resFile.close();
 
-//FIXME should really find raceName in the AthList
-// I do, this is legacy
     // Find each raceName in the DBnames list
     long raceSize = raceName.size();
-    long dbSize = name.size();
-    cout << raceSize << " " << dbSize << endl;
-
-    for ( long i = 0; i < raceSize; i++ ) {
-        long j;
-        // Iterate to find the matching name
-        for ( j = 0; j < dbSize; j++ ) {
-            if ( raceName[i] == name[j] ) {
-                cout << "raceName " << raceName[i] << " dbName " << name[j] << endl;
-                break;
-            }
-        }
-
-        vector<float>::iterator dbPos = points[j].begin() + 1;
-        for ( int col = 1; col < points[j].size(); col++ ) {
-            if ( racePoints[i] > points[j][col] ) {
-                //insert the race points into the vector here
-                points[j].insert( dbPos, racePoints[i] );
-                if ( points[j].size() > 5 ) {
-                    points[j].pop_back();
-                }
-                break;
-            }
-            dbPos++;
-        }
-    }
-
-    cout << "\n";
 
     // Find each racename in the athList
     long athSize = athList.size();
+    cout << raceSize << " " << athSize << endl;
     for ( long i = 0; i < raceSize; i++ ) {
         long j;
         for ( j = 0; j < athSize; j++ ) {
             if ( raceName[i] == athList[j].getSearchName() ) {
-                cout << "raceName " << raceName[i] << " athName " << athList[j].getSearchName() << endl;
+//                cout << "raceName " << raceName[i] << " athName " << athList[j].getSearchName() << endl;
                 athList[j].givePoints( racePoints[i] );
                 break;
             }
@@ -187,68 +151,36 @@ int main( void ) {
 
     // Average all the rows into the first points column
     // Also compute the scaling factor necessary to make the average DB score to be 1000
-    float dbSum = 0;
-    dbSize = name.size();
-    for ( long j = 0; j < dbSize; j++ ) {
-        float sum = 0;
-        for ( int i = 0; i < points[j].size(); i++ ) {
-            sum += points[j][i];
-        }
-        points[j][0] = sum / points[j].size();
-        dbSum += points[j][0];
-    }
-
     for ( int i = 0; i < athList.size(); i++ ) {
         athList[i].print();
     }
+    cout << "\n";
     float athSum = 0;
     for ( long i = 0; i < athList.size(); i++ ) {
         athSum += athList[i].getRankPoints();
     }
     // Factor to scale the average to 1000
-    float dbScale = 1000 / ( dbSum / dbSize );
     float athScale = 1000 / ( athSum / athSize );
-    cout << "dbScale " << dbScale << " is it 1000? " <<  ( dbSum / dbSize ) * dbScale << endl;
     cout << "athScale " << athScale << " is it 1000? " <<  ( athSum / athSize ) * athScale << endl;
+
+//    assert( ( athSum / athSize ) * athScale - 1000 < 1e-6 );
 
     for ( long j = 0; j < athSize; j++ ) {
         athList[j].scaleAllPoints( athScale );
-        athList[j].print();
     }
 
     // Sort the DB after scaling
     std::sort( athList.begin(), athList.end() );
 
-    for ( long j = 0; j < dbSize; j++ ) {
-        cout << "finalDB " << name[j];
-        for ( int i = 0; i < points[j].size(); i++ ) {
-            points[j][i] *= dbScale;
-            cout << " " << points[j][i];
-        }
-        cout << endl;
-    }
-
     cout << "finalDB ";
     for ( int i = 0; i < athList.size(); i++ ) {
         athList[i].print();
     }
+    cout << "\n";
 
-
-    // Overwrite the old DB with the new one.
-    ofstream newDBFile ("newPointsDB");
-
-    for ( long j = 0; j < dbSize; j++ ) {
-        newDBFile << fullName[j] << ";";
-        for ( int i = 0; i < points[j].size(); i++ ) {
-            newDBFile << points[j][i] << ";";
-        }
-        newDBFile << "\n";
-    }
-
-    newDBFile.close();
-
+    // Create the new DB.
     ofstream newAthDBFile ("newAthPointsDB");
-    for ( long j = 0; j < dbSize; j++ ) {
+    for ( long j = 0; j < athSize; j++ ) {
         athList[j].printToDbFile( newAthDBFile );
     }
     newAthDBFile.close();
